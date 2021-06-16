d576113496d701e794d6332b402b41f78449c320
Aya Maher <aya.nassan2000@gmail.com>
2021-06-17 00:10:49 +0300

    commit A

--- app/src/main/java/model/AboutAdapter.java
+++ app/src/main/java/model/AboutAdapter.java
@@ -5,14 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.cloudproject.R;
 import com.squareup.picasso.Picasso;
-
 import java.util.List;
+
 
 public class AboutAdapter extends RecyclerView.Adapter<AboutAdapter.photoViewHolder> {
 

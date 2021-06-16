b43e7be9d24809f3e7a9cebba7bdbe08e6738f5c
Aya Maher <aya.nassan2000@gmail.com>
2021-06-16 15:27:06 +0300

    commit A

---
+++ app/src/main/java/Activities/SignIn.java
@@ -0,0 +1,17 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cloudproject.R;
+
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
